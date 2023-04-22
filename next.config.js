0c81d9f3fa2c7f9aa0d14497b66c925f9c95aae5
pdinh123 <86795020+pdinh123@users.noreply.github.com>
2023-04-22 14:31:05 -0400

    trying again

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_ASSET_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
