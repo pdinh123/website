3d435b9087d60d648198af4f8d582c9eb917f464
pdinh123 <86795020+pdinh123@users.noreply.github.com>
2023-04-22 15:15:35 -0400

    trying asset prefix

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const debug = process.env.NODE_ENV !== 'production';
+
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: !debug ? 'https://pdinh123.github.io/website/' : '',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
