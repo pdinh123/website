aaf72aff6f8c906ff4a72c040ce449103ea8c7d7
pdinh123 <86795020+pdinh123@users.noreply.github.com>
2023-04-22 12:22:21 -0400

    try env variables

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
-  assetPrefix: '/pdinh123.github.io',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
