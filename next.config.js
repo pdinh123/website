e57af59c754a4aa6d85f916c8d119fb82251d84e
pdinh123 <86795020+pdinh123@users.noreply.github.com>
2023-04-21 23:40:18 -0400

    fixed actions for github

--- next.config.js
+++ next.config.js
@@ -3,6 +3,24 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix,
+  basePath,
+};
+
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
